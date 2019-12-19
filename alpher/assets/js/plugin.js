4bfd19e05e08ca4926d8cb482f00debde672e447
NATFINEST <ese.nat4@gmail.com>
2019-12-19 12:32:38 +0100

    up

--- alpher/assets/js/plugin.js
+++ alpher/assets/js/plugin.js
@@ -364,10 +364,22 @@
     prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></button>',
     nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>',
     speed: 300,
-    slidesToShow: 3,
+    slidesToShow: 4,
     slidesToScroll: 1,
     responsive: [{
         breakpoint: 1500,
+        settings: {
+          arrows: false,
+          dots: false,
+          infinite: true,
+          autoplay: false,
+          fade: false,
+          speed: 300,
+          slidesToShow: 4,
+          slidesToScroll: 1
+        }
+      }, {
+        breakpoint: 1350,
         settings: {
           arrows: true,
           dots: false,
@@ -375,25 +387,13 @@
           autoplay: false,
           fade: false,
           speed: 300,
-          slidesToShow: 3,
-          slidesToScroll: 1
-        }
-      }, {
-        breakpoint: 1350,
-        settings: {
-          arrows: false,
-          dots: false,
-          infinite: true,
-          autoplay: false,
-          fade: false,
-          speed: 300,
-          slidesToShow: 3,
+          slidesToShow: 4,
           slidesToScroll: 1
         }
       }, {
         breakpoint: 992,
         settings: {
-          arrows: false,
+          arrows: true,
           dots: false,
           infinite: true,
           autoplay: false,
@@ -405,7 +405,7 @@
       }, {
         breakpoint: 768,
         settings: {
-          arrows: false,
+          arrows: true,
           dots: false,
           infinite: true,
           autoplay: false,
@@ -493,7 +493,7 @@
     prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></button>',
     nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>',
     speed: 300,
-    slidesToShow: 3,
+    slidesToShow: 4,
     slidesToScroll: 1,
     responsive: [{
         breakpoint: 1500,
