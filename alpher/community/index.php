e274f68d22c9317eda442b04c8b5437aeb514ddf
NATFINEST <ese.nat4@gmail.com>
2020-01-07 01:18:29 +0100

    Update index.php

--- alpher/community/index.php
+++ alpher/community/index.php
@@ -1,7 +1,5 @@
 <?php
 
-
-    <?php
     if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
         header('Access-Control-Allow-Origin: *');
         header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
