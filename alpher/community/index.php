4fec2ff304a81d93826d095f5ee3a10f197a7cac
NATFINEST <ese.nat4@gmail.com>
2020-01-07 01:17:42 +0100

    Update index.php

--- alpher/community/index.php
+++ alpher/community/index.php
@@ -1,11 +1,26 @@
 <?php
 
 
-    header('Access-Control-Allow-Origin: *'); 
-    header("Access-Control-Allow-Credentials: true");
-    header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
-    header('Access-Control-Max-Age: 1000');
-    header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
+        header('Access-Control-Allow-Headers: token, Content-Type');
+        header('Access-Control-Max-Age: 1728000');
+        header('Content-Length: 0');
+        header('Content-Type: text/plain');
+        die();
+    }
+
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+
+    $ret = [
+        'result' => 'OK',
+    ];
+    print json_encode($ret);
+
+    echo "Works";
 
     echo $_POST["name"];
 ?>
