5dff2ce81a12458da3006e7599ba2bb453e16f27
NATFINEST <ese.nat4@gmail.com>
2020-05-17 23:54:13 +0100

    up

--- alpher/new/js/utility.js
+++ alpher/new/js/utility.js
@@ -602,9 +602,58 @@ function homePageOnly(){
 $(document).ready(function() {
     $('.chat-conversation').hide();
 
+
+    function formatRepo (repo) {
+      if (repo.loading) {
+        return repo.text;
+      }
+
+      var $container = $(
+        "<div class='select2-result-repository clearfix'>" +
+            "<div class='select2-result-repository__title'></div>" +
+            "</div>" +
+          "</div>" +
+        "</div>"
+      );
+
+      $container.find(".select2-result-repository__title").text(repo.first_name);
+
+      return $container;
+    }
+
+    function formatRepoSelection (repo) {
+      return repo.first_name || repo.text;
+    }
+
     $('.js-example-basic-single').select2({
         placeholder: "Select a contact to start a conversation",
         dropdownParent: $("#compose"),
+          ajax: {
+            url: "https://reqres.in/api/users",
+            dataType: 'json',
+            delay: 250,
+            data: function (params) {
+              return {
+                q: params.term, // search term
+                page: params.page
+              };
+            },
+            processResults: function (data, params) {
+              params.page = params.page || 1;
+              return {
+                results: data.data,
+                pagination: {
+                  more: (params.page * 30) < data.total_count
+                }
+              };
+            },
+            cache: true
+          },
+          placeholder: 'Search for a repository',
+          minimumInputLength: 1,
+          templateResult: formatRepo,
+          templateSelection: formatRepoSelection
+
     }).on("change", function(e) {
         //Initialize variables
         name = $("#select-contact").select2('data')[0].element.dataset['fullName'];
