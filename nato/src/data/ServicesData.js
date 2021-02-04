216ebd4f0f8ec4f07ef37af81f79aeb9b4e42580
NATFINEST <ese.nat4@gmail.com>
2021-02-04 04:43:27 +0000

    up

--- nato/src/data/ServicesData.js
+++ nato/src/data/ServicesData.js
@@ -1,20 +1,23 @@
 export const servicesData = [
   {
     title: "Website Development",
-    icon: "",
+    icon: "fab fa-react",
+    image: "/images/website.jpg",
     description:
       "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout",
   },
   {
     title: "Graphics Design",
-    icon: "",
+    icon: "fa fa-adjust",
+    image: "/images/graphics.jpg",
     description:
       "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout",
   },
   {
     title: "Motion Graphics",
-    icon: "",
+    icon: "fab fa-uikit",
+    image: "/images/motion.jpeg",
     description:
       "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout",
   },
-]
+];
